282fc6afc805f9bfa7d5fd551b08cd85642ea832
abdullahalwafi <abdullahalwafi16@gmail.com>
2025-08-20 15:24:43 +0700

    fix: landing auto redirect to login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <meta http-equiv="refresh" content="0; url={{ url('/admin') }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
